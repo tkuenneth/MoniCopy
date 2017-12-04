b9b3e543d87c8520a72183e3cc4d3a210ab0b0ef
Thomas Künneth <thomas.kuenneth@mathema.de>
2017-12-04 14:36:04 +0100

    added license header; added folder selection

--- src/com/thomaskuenneth/monicopy/Main.java
+++ src/com/thomaskuenneth/monicopy/Main.java
@@ -1,13 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2017 Thomas Kuenneth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.thomaskuenneth.monicopy;
 
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.prefs.Preferences;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -15,19 +26,23 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 /**
  *
- * @author tkuen
+ * @author Thomas Kuenneth
  */
 public class Main extends Application {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static final String KEY_FILE_FROM = "fileFrom";
+    private static final String KEY_FILE_TO = "fileTo";
 
     private static TextArea ta = null;
 
@@ -40,13 +55,37 @@ public class Main extends Application {
     private final MD5 md2 = new MD5();
     private final StringBuilder sb2 = new StringBuilder();
 
+    private final Preferences prefs = Preferences.userNodeForPackage(Main.class);
+
+    private Stage primaryStage = null;
+    private File fileFrom = null;
+    private File fileTo = null;
+    private Button button = null;
+
     @Override
     public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        fileFrom = getFileFromPreferences(KEY_FILE_FROM);
+        fileTo = getFileFromPreferences(KEY_FILE_TO);
         Text t1 = new Text("Copy all files and folders inside");
-        Text t2 = new Text("...");
+        Hyperlink t2 = new Hyperlink();
+        updateHyperLink(t2, fileFrom);
+        t2.setOnAction((ActionEvent event) -> {
+            fileFrom = selectDir(fileFrom);
+            updateHyperLink(t2, fileFrom);
+            updateCopyButton();
+            setPreferencesFromFile(fileFrom, KEY_FILE_FROM);
+        });
         Text t3 = new Text("to");
-        Text t4 = new Text("...");
-        Button button = new Button("Start");
+        Hyperlink t4 = new Hyperlink();
+        updateHyperLink(t4, fileTo);
+        t4.setOnAction((ActionEvent event) -> {
+            fileTo = selectDir(fileTo);
+            updateHyperLink(t4, fileTo);
+            updateCopyButton();
+            setPreferencesFromFile(fileTo, KEY_FILE_TO);
+        });
+        button = new Button("Start");
         button.setOnAction((ActionEvent event) -> {
             ta = new TextArea();
             VBox root = new VBox(ta);
@@ -54,13 +93,13 @@ public class Main extends Application {
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
 
-            File from = new File("C:\\Users\\tkuen");
-            File to = new File("D:\\");
             Thread t = new Thread(() -> {
-                copy(from, to);
+                copy(fileFrom, fileTo);
             });
             t.start();
         });
+        updateCopyButton();
+
         HBox box = new HBox(button);
         box.setAlignment(Pos.CENTER);
 
@@ -92,6 +131,39 @@ public class Main extends Application {
                 }
             }
         }
+    }
+
+    private File getFileFromPreferences(String key) {
+        File result = null;
+        String val = prefs.get(key, "");
+        if ((val != null) && (val.length() > 0)) {
+            result = new File(val);
+        }
+        return result;
+    }
+
+    private void setPreferencesFromFile(File dir, String key) {
+        String val = "";
+        if (dir != null) {
+            val = dir.getAbsolutePath();
+        }
+        prefs.put(key, val);
+    }
+
+    private File selectDir(File current) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("JavaFX Projects");
+        chooser.setInitialDirectory(current);
+        File selectedDirectory = chooser.showDialog(primaryStage);
+        return selectedDirectory;
+    }
+
+    private void updateHyperLink(Hyperlink link, File dir) {
+        link.setText(dir == null ? "\u2026" : dir.getAbsolutePath());
+    }
+
+    private void updateCopyButton() {
+        button.setDisable(fileFrom == null || fileTo == null);
     }
 
     private synchronized boolean mustBeCopied(File fileToCopy, File destination) {
