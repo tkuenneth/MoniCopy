87710a324d6addbb07a5b0cfb3a89d06141a607b
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-07-24 19:19:25 +0200

    Windows script added

---
+++ jpackage_win.ps1
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+$JAVA_HOME = "C:\Program Files\Java\jdk-14.0.2"
+$BASEDIR = "C:\Users\tkuen\Entwicklung\GitHub\MoniCopy"
+$VERSION = "1.0.0" # `sed -n -e 's/.*VERSION = \"\(.*\)\".*/\1/p' < $BASEDIR/src/com/thomaskuenneth/monicopy/Main.java`
+
+Write-Output "JAVA_HOME: $JAVA_HOME"
+Write-Output "BASEDIR: $BASEDIR"
+Write-Output "Version: $VERSION"
+
+#rmdir -r "$BASEDIR\MoniCopy"
+
+$COMMAND = "$JAVA_HOME\bin\jpackage.exe"
+$ARGUMENTS = "--name MoniCopy --icon $BASEDIR\artwork\MoniCopy.ico --type msi --app-version $VERSION --module-path $BASEDIR\dist\MoniCopy.jar;`"C:\Program Files\Java\javafx-jmods-13.0.2`" -m main/com.thomaskuenneth.monicopy.Main"
+
+echo $ARGUMENTS
+
+Start-Process -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -FilePath $COMMAND -ArgumentList $ARGUMENTS -Wait
