6f0026f4793746a1e41c924fbf7e74b809e8b135
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-07-25 12:10:47 +0200

    Update jpackage_win.ps1

--- jpackage_win.ps1
+++ jpackage_win.ps1
@@ -1,18 +1,22 @@
-#!/bin/sh
+$java_home = "C:\Program Files\Java\jdk-14.0.2"
+$base_dir = "C:\Users\tkuen\Entwicklung\GitHub\MoniCopy"
 
-$JAVA_HOME = "C:\Program Files\Java\jdk-14.0.2"
-$BASEDIR = "C:\Users\tkuen\Entwicklung\GitHub\MoniCopy"
-$VERSION = "1.0.0" # `sed -n -e 's/.*VERSION = \"\(.*\)\".*/\1/p' < $BASEDIR/src/com/thomaskuenneth/monicopy/Main.java`
+$version = "???"
+$source = Get-Content -Path $base_dir\src\com\thomaskuenneth\monicopy\Main.java
+foreach($line in $source) {
+    if ($line -match "VERSION = `"(.+)`"") {
+        $version = $matches[1]
+        break
+    }
+}
 
-Write-Output "JAVA_HOME: $JAVA_HOME"
-Write-Output "BASEDIR: $BASEDIR"
-Write-Output "Version: $VERSION"
+Write-Output "java_home: $java_home"
+Write-Output "base_dir: $base_dir"
+Write-Output "version: $version"
 
-#rmdir -r "$BASEDIR\MoniCopy"
+$command = "$java_home\bin\jpackage.exe"
+$arguments = "--win-menu --win-menu-group `"Thomas Kuenneth`" --vendor `"Thomas Kuenneth`" --name MoniCopy --icon $base_dir\artwork\MoniCopy.ico --type msi --app-version $version --module-path $base_dir\dist\MoniCopy.jar;`"C:\Program Files\Java\javafx-jmods-13.0.2`" -m main/com.thomaskuenneth.monicopy.Main"
 
-$COMMAND = "$JAVA_HOME\bin\jpackage.exe"
-$ARGUMENTS = "--win-menu --win-menu-group `"Thomas Kuenneth`" --vendor `"Thomas Kuenneth`" --name MoniCopy --icon $BASEDIR\artwork\MoniCopy.ico --type msi --app-version $VERSION --module-path $BASEDIR\dist\MoniCopy.jar;`"C:\Program Files\Java\javafx-jmods-13.0.2`" -m main/com.thomaskuenneth.monicopy.Main"
+Write-Output $arguments
 
-echo $ARGUMENTS
-
-Start-Process -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -FilePath $COMMAND -ArgumentList $ARGUMENTS -Wait
+Start-Process -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -FilePath $command -ArgumentList $arguments -Wait
