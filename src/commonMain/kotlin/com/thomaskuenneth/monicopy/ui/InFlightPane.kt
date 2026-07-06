8c3525941f8f26dbfff5f3ad85220c2a20e9b59d
Thomas Künneth <kuennetht@gmail.com>
2026-07-06 12:25:14 +0200

    UI refinement

--- src/commonMain/kotlin/com/thomaskuenneth/monicopy/ui/InFlightPane.kt
+++ src/commonMain/kotlin/com/thomaskuenneth/monicopy/ui/InFlightPane.kt
@@ -1,14 +1,19 @@
 package com.thomaskuenneth.monicopy.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontFamily
 
 @Composable
@@ -23,7 +28,15 @@ fun InFlightPane(
     }
     LazyColumn(
         state = listState,
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.surface)
+            .border(CardDefaults.outlinedCardBorder(), MaterialTheme.shapes.medium),
+        contentPadding = PaddingValues(
+            horizontal = UIConstants.SMALL_HORIZONTAL_PADDING,
+            vertical = UIConstants.SMALL_VERTICAL_PADDING,
+        ),
     ) {
         items(logMessages, key = { it }) { line ->
             Text(
